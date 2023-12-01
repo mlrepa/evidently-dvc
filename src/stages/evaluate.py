d2900a02b4725af7e2c9a365695e8e5d654b168f
Mikhail <mnrozhkov@gmail.com>
2023-12-01 15:14:19 +0100

    Remove unused code from evaluate.py

--- src/stages/evaluate.py
+++ src/stages/evaluate.py
@@ -18,25 +18,6 @@ from evidently.metrics import (
 )
 from evidently.report import Report
 
-def numpy_to_standard_types(input_data: Dict) -> Dict:
-    """Convert numpy type values to standard Python types in flat(!) dictionary.
-
-    Args:
-        input_data (Dict): Input data (flat dictionary).
-
-    Returns:
-        Dict: Dictionary with standard value types.
-    """
-
-    output_data: Dict = {}
-
-    for k, v in input_data.items():
-        if isinstance(v, np.generic):
-            v = v.item()
-        output_data[k] = v
-
-    return output_data
-
 
 
 def evaluate(config_path: Text) -> None:
